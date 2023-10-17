64f2362007cee789e6fc055ef5cc79a551ee3a9d
MuchanKim <mutopia82@gmail.com>
2023-10-18 01:12:58 +0900

    update - fastapi 연동

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import shutil
 
-# Create a FastAPI app instance
 app = FastAPI()
 
-# Define an endpoint for "/hello"
-@app.get("/hello")
-def read_root():
-    return {"message": "Hello, world!"}
+@app.post("/upload")
+async def upload_image(image: UploadFile):
+    try:
+        with open(f"uploaded_{image.filename}", "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
+        return JSONResponse(content={"message": "Image uploaded successfully"})
+    except Exception as e:
+        return JSONResponse(content={"message": f"Failed to upload image: {str(e)}"})
 
-# Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
 
--- frontend/lib/camera_ex.dart
+++ frontend/lib/camera_ex.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+// 
+import 'package:http/http.dart' as http;
 
 class CameraExample extends StatefulWidget {
   const CameraExample({Key? key}) : super(key: key);
@@ -11,15 +13,41 @@ class CameraExample extends StatefulWidget {
 }
 
 class _CameraExampleState extends State<CameraExample> {
-  File? _image;
+  File? _image; //  이미지 파일을 저장할 변수
   final picker = ImagePicker();
 
   Future getImage(ImageSource imageSource) async {
-    final image = await picker.pickImage(source: imageSource);
+    final image = await picker.pickImage(source: imageSource);  // 카메라 또는
 
     setState(() {
-      _image = File(image!.path);
+      _image = File(image!.path);  // 선택한 이미지로 변수 업데이트
     });
+
+    // 선택한 이미지를 FastAPI 서버로 업로드
+    uploadImage();
+  }
+
+  Future uploadImage() async {
+    if (_image == null) {
+      print('No image selected.');  // 이미지가 선택되지 않았을 경우 로그 출력
+      return; 
+    }
+
+    final uri = Uri.parse('http://127.0.0.1:8000/upload');  // FastAPI 엔드포인트 URL
+    final request = http.MultipartRequest('POST', uri)
+      ..files.add(await http.MultipartFile.fromPath('image', _image!.path));
+
+    try {
+      final response = await request.send();  // 이미지 업로드 시도
+
+      if (response.statusCode == 200) {
+        print('Image uploaded successfully');  // 성공적으로 업로드됐을 경우 로그 출력
+      } else {
+        print('Failed to upload image');  // 업로드 실패 시 로그 출력
+      }
+    } catch (e) {
+      print('Error during image upload: $e');  // 업로드 중 에러 발생 시 로그 출력
+    }
   }
 
   @override
